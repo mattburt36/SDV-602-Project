35650a57fd3dbbecc1725a071c8d99898692b21a
Matt-PCHome <36slices@gmail.com>
2022-10-17 10:50:02 +1300

    Layout of controller and view done

--- GUIV2/Controller.py
+++ GUIV2/Controller.py
@@ -1,4 +1,39 @@
 """
 This file houses the controls the user will utilise when the application is being executed (the main for loop for the GUI)\
 This file has scope of the View.py file to 'watch' what users are doing with what information 
+
+This file is where the code execution begins/ends, start execution here 
 """
+import Common as c 
+import View as v
+
+#-----------------------------------------------------------------------------------------------------
+# Instantiate the window 
+window = v.create_window()
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize the main loop
+if __name__ == "__main__":
+    while True: 
+        #---------------------------------------------------------------------------------------------
+        # Assign the events variable to window.read() to track event and value changes on the GUI 
+        event, values = window.read()
+
+        #---------------------------------------------------------------------------------------------
+        # Regardless of state, given that the form is closed, exit the main loop and application 
+        if event == c.sg.WIN_CLOSED:
+            break
+
+        #---------------------------------------------------------------------------------------------
+        # Run a constant update on the graph being displayed, based on changes of any kind in the GUI
+
+        #---------------------------------------------------------------------------------------------
+        # Run state machine check on window_flag to control different events 
+        #   - Login window:         window_flag == 0 
+        #   - Registration window:  window_flag == 1
+        #   - Graph window:         window_flag == 2
+        #   - Map window:           window_flag == 3
+        
+    #-------------------------------------------------------------------------------------------------
+    # Default to no window open once all conditions are satisfied 
+    window.close()
--- GUIV2/View.py
+++ GUIV2/View.py
@@ -1,3 +1,17 @@
 """
 This file contains the software which presents information to the user
+It has the scope of the Model.py file, to recieve what information to be displayed to the user 
 """
+
+import Common as c 
+import Model as m 
+
+def draw_graph():
+    return 
+    
+def create_plot():
+    return 
+
+def create_window():
+    return
+
